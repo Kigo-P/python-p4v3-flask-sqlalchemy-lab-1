616ba1fbfb7e26e79dad6cb6d01a7cc7a5acb80f
Kigo-P <peterkigo33@gmail.com>
2024-10-02 15:54:26 +0300

    completed the lab

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 # server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
 
 from models import db, Earthquake
@@ -20,7 +20,47 @@ def index():
     body = {'message': 'Flask SQLAlchemy Lab 1'}
     return make_response(body, 200)
 
-# Add views here
+# Add views here   
+@app.route("/earthquakes/<int:id>",methods=["GET"])
+def earthquakes(id):
+    get_earthquake = Earthquake.query.filter(Earthquake.id == id).first()
+    print(get_earthquake)
+    if get_earthquake:
+        response = {
+        "id" : get_earthquake.id,
+        "magnitude" : get_earthquake.magnitude,
+        "location" : get_earthquake.location,
+        "year": get_earthquake.year
+        }
+        return jsonify(response), 200   
+    else:
+        response = {
+            "message": f"Earthquake {id} not found."
+        }
+        return jsonify(response), 404
+
+@app.route("/earthquakes/magnitude/<float:magnitude>", methods=["GET"])
+def get_magnitude(magnitude):
+    get_all_earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    
+
+    quake = [{
+        "id" : get_all_earthquake.id,
+        "magnitude" : get_all_earthquake.magnitude,
+        "location" : get_all_earthquake.location,
+        "year": get_all_earthquake.year
+        } for get_all_earthquake in get_all_earthquakes]
+
+
+
+    return jsonify({
+        "count": len(quake),
+        "quakes": quake
+    }), 200 
+    
+
+
+
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -7,3 +7,16 @@ metadata = MetaData()
 db = SQLAlchemy(metadata=metadata)
 
 # Add models here
+class Earthquake(db.Model,SerializerMixin):
+    # creating a table called earthquakes
+    __tablename__ = "earthquakes"
+    # creating columns with their data types
+    id = db.Column(db.Integer, primary_key=True)
+    magnitude = db.Column(db.Float, nullable=False)
+    location = db.Column(db.String, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+
+    # a repr method to represent the string format 
+    def __repr__(self) -> str:
+        return f"<Earthquake {self.id}, {self.magnitude}, {self.location}, {self.year}>"
+    pass
